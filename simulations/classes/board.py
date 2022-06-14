1823da903443348146346d27bbead72b15a2e314
Dee Lalwani <deepplalwani@hotmail.com>
2022-06-14 22:10:50 +1200

    board representation

--- simulations/classes/board.py
+++ simulations/classes/board.py
@@ -5,6 +5,7 @@ from manufacturerList import ManufacturerList
 
 @dataclass()
 class Board():
+    name: str
     manufacturer: str
     pin_size: int
     debugger_available: Optional[bool] = False;
@@ -14,9 +15,12 @@ class Board():
         if(self.manufacturer == ManufacturerList.ATMEL):
             self.debugger_available = True;
 
+    def __repr__(self):
+        return f"ID:{self.id:2}   |   {self.name:10} |   {self.manufacturer:40}  |   ({self.pin_size})"
 
-ATMEGA328P = Board(manufacturer=ManufacturerList.ATMEL, pin_size=16);
-MPU6050 = Board(manufacturer=ManufacturerList.TEXAS_INSTRUMENTS, pin_size=6);
+
+ATMEGA328P = Board(name="ATMEGA328P", manufacturer=ManufacturerList.ATMEL, pin_size=16);
+MPU6050 = Board(name="MPU6050",manufacturer=ManufacturerList.TEXAS_INSTRUMENTS, pin_size=6);
 
 print(ATMEGA328P)
 print(MPU6050)
